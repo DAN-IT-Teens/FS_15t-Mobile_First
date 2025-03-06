3aad8597f09eb6fdaf57da81c48ae1a6da97bc7b
Kirill Nagovitsyn <kirill.nagovitsyn2009@gmail.com>
2025-03-06 18:58:22 +0200

    Додали меню

--- settings/tasks/script.js
+++ settings/tasks/script.js
@@ -1,8 +1,11 @@
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+
 
 function scriptTask() {
     return gulp.src("./src/scripts/*.js")
+        .pipe(babel({ presets: ['@babel/preset-env'] }))
         .pipe(uglify())
         .pipe(gulp.dest('build/scripts'))
 }
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,8 @@
-console.log('Hello world!');
+const menuButton = document.querySelector(".header__menu--button");
+const mobileMenu = document.querySelector(".header__menu");
+
+menuButton.addEventListener('click', function () {
+    mobileMenu.classList.toggle("open");
+
+    
+})
